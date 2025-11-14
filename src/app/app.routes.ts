f995a56edd6d4274639a4b14ef9a2de635774781
SheHab970 <shehab202205097@gmail.com>
2025-11-14 15:30:55 +0200

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+ {
+    path: 'home',
+    loadComponent: () =>
+      import('./modules/pages/home/home').then(
+        (c) => c.Home,
+      ),
+  },
+ {
+    path: 'about',
+    loadComponent: () =>
+      import('./modules/pages/about/about').then(
+        (c) => c.About,
+      ),
+  },
+ {
+    path: 'projects',
+    loadComponent: () =>
+      import('./modules/pages/projects/projects').then(
+        (c) => c.Projects,
+      ),
+  },
+ {
+    path: 'services',
+    loadComponent: () =>
+      import('./modules/pages/services/services').then(
+        (c) => c.Services,
+      ),
+  },
+ {
+    path: 'contact',
+    loadComponent: () =>
+      import('./modules/pages/contact/contact').then(
+        (c) => c.Contact,
+      ),
+  },
+];
