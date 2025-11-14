d77145416cc4ed9aec80dade5ceeeb49a694128a
SheHab970 <shehab202205097@gmail.com>
2025-11-14 23:44:43 +0200

    Add footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './core/nav-bar/nav-bar';
+import { Footer } from './core/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavBar],
+  imports: [RouterOutlet,NavBar,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
